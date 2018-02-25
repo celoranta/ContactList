7c3ec3fa620a46dd67c0d7147107dd4b5060d88f
Christoper <celoranta@gmail.com>
2018-02-25 12:30:41 -0800

    Initial Commit

---
+++ ContactList/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ContactList
+//
+//  Created by Chris Eloranta on 2018-02-25.
+//  Copyright © 2018 Christopher Eloranta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
